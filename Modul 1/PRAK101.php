af1e0030a7bf2fee160a86584bee22df507f3d0e
frdvndb <89407930+frdvndb@users.noreply.github.com>
2023-03-18 19:26:03 +0700

    Delete Modul 1 directory

--- Modul 1/PRAK101.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Penggunaan fungsi echo untuk menampilkan string
-echo "Hello World in PHP<br>";
-echo "Nama Praktikan : M. Farid Pebrian<br>";
-echo "NIM : 2110817210015";
-?>
--- Modul 1/PRAK102.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Dengan angka NIM akhir 5, maka digunakan bangun ruang bola
-// Kalkulasi volume bola menggunakan rumusnya
-$jari2 = 4.2;
-$VolumeBola = 4/3 * 3.14 * 4.2 * 4.2 * 4.2;
-
-// penggunaan echo untuk menampilkan data dan fungsi number_format untuk mengubah titik pada nilai menjadi koma
-echo number_format($VolumeBola, 3,",","."),  " m3"; 
-?>
